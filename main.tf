a515a221fd8c0826c48ae2415fb4e48673e34fb9
Jessie Hernandez <jessie.hernandez@protonmail.com>
2021-07-12 15:41:16 -0500

    Added several more Lambda configuration variables related to security settings.

--- main.tf
+++ main.tf
@@ -6,20 +6,30 @@ locals {
 module "rotator_lambda" {
   source = "terraform-aws-modules/lambda/aws"
 
-  attach_network_policy  = true
-  create_package         = false
-  create_role            = var.create_role
-  description            = var.description
-  function_name          = var.name
-  handler                = "notneeded"
-  lambda_role            = var.lambda_role
-  local_existing_package = local.package_file
-  memory_size            = 128
-  runtime                = "provided.al2"
-  tags                   = var.tags
-  timeout                = 180
-  vpc_security_group_ids = var.vpc_security_group_ids
-  vpc_subnet_ids         = var.vpc_subnet_ids
+  attach_network_policy             = true
+  cloudwatch_logs_kms_key_id        = var.cloudwatch_logs_kms_key_id
+  cloudwatch_logs_retention_in_days = var.cloudwatch_logs_retention_in_days
+  cloudwatch_logs_tags              = var.cloudwatch_logs_tags
+  create_package                    = false
+  create_role                       = var.create_role
+  description                       = var.description
+  function_name                     = var.name
+  handler                           = "notneeded"
+  kms_key_arn                       = var.kms_key_arn
+  lambda_role                       = var.lambda_role
+  local_existing_package            = local.package_file
+  memory_size                       = 128
+  role_description                  = var.role_description
+  role_name                         = var.role_name
+  role_path                         = var.role_path
+  role_permissions_boundary         = var.role_permissions_boundary
+  role_tags                         = var.role_tags
+  runtime                           = "provided.al2"
+  tags                              = var.tags
+  timeout                           = 180
+  use_existing_cloudwatch_log_group = var.use_existing_cloudwatch_log_group
+  vpc_security_group_ids            = var.vpc_security_group_ids
+  vpc_subnet_ids                    = var.vpc_subnet_ids
 }
 
 resource "aws_iam_role_policy" "rotator_lambda" {
--- variables.tf
+++ variables.tf
@@ -11,6 +11,24 @@ variable "vpc_subnet_ids" {
 # OPTIONAL PARAMETERS
 # ------------------------------------------------------------------------------
 
+variable "cloudwatch_logs_kms_key_id" {
+  default     = null
+  description = "The ARN of the KMS key to use when encrypting log data."
+  type        = string
+}
+
+variable "cloudwatch_logs_retention_in_days" {
+  default     = null
+  description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
+  type        = number
+}
+
+variable "cloudwatch_logs_tags" {
+  default     = {}
+  description = "A map of tags to assign to the CloudWatch logs."
+  type        = map(string)
+}
+
 variable "create_role" {
   default     = true
   description = "Controls whether an IAM role for the rotator Lambda should be created."
@@ -18,9 +36,15 @@ variable "create_role" {
 }
 
 variable "description" {
-    default     = "Database password rotator."
-    description = "Description of the rotator Lambda function."
-    type        = string
+  default     = "Database password rotator."
+  description = "Description of the rotator Lambda function."
+  type        = string
+}
+
+variable "kms_key_arn" {
+  default     = null
+  description = "The ARN of the KMS key to use for the rotator Lambda function."
+  type        = string
 }
 
 variable "lambda_role" {
@@ -35,10 +59,46 @@ variable "name" {
   type        = string
 }
 
+variable "role_description" {
+  default     = null
+  description = "Description of the IAM role to use for the rotator Lambda function when creating the role via this module."
+  type        = string
+}
+
+variable "role_name" {
+  default     = null
+  description = "Name of the IAM role to use for the rotator Lambda function when creating the role via this module."
+  type        = string
+}
+
+variable "role_path" {
+  default     = null
+  description = "Path of IAM role to use for the rotator Lambda function when creating the role via this module."
+  type        = string
+}
+
+variable "role_permissions_boundary" {
+  description = "The ARN of the policy that is used to set the permissions boundary for the IAM role used by the rotator Lambda function when creating the role via this module."
+  type        = string
+  default     = null
+}
+
+variable "role_tags" {
+  default     = {}
+  description = "A map of tags to assign to the IAM role when creating the role via this module."
+  type        = map(string)
+}
+
 variable "tags" {
   default     = {}
   description = "Tags to apply to the Lambda function."
   type        = map(string)
+}
+
+variable "use_existing_cloudwatch_log_group" {
+  default     = false
+  description = "Whether to use an existing CloudWatch log group or create a new one."
+  type        = bool
 }
 
 variable "vpc_security_group_ids" {
